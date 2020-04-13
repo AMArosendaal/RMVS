7aec9e168361ceffe76330dc8ad6c93635839c24
Alex <alexrosendaal@gmail.com>
2020-04-13 18:09:10 +0200

    Monitoring frequency

--- src/main.cpp
+++ src/main.cpp
@@ -3,16 +3,24 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 #include <toneAC.h>
-#include "Plotter.h"
 
 #define OLED_ADDR   0x3C  // OLED display TWI address
 #define SCREEN_WIDTH 128  // OLED display width, in pixels
 #define SCREEN_HEIGHT 64  // OLED display height, in pixels
 
-// *** Real INPUTS
-const int pressurePin = A0;       // (A0) select the input pin for potMeter A  
-// *** VIRTUAL INPUTS
+// smoothed z-score algorithm
+const int lag = 30; // lag-1 (5) for the smoothing functions
+const float threshold = 7; //3.5 standard deviations for signal
+float influence = 0.5; //between 0 and 1, where 1 is normal influence, 0.5 is half
+float floatArray[50];
+float mean; 
+float stdev;
+float z_score;
+int signal;
+long lastInsp = 0;
 
+// *** VIRTUAL INPUTS
+const int pressurePin = A0;       // (A0) select the input pin for potMeter A  
 const int pressurePeriodPin = A1; // (A1) select the input pin for potMeter B
 const int  batteryVoltagePin = A2; // (A2) select the input pin for potMeter C
 const int  xPowerPin = A3;         // (A3) select the input for Potmeter D 
@@ -24,12 +32,11 @@ const int gLedPin = 4;
 const int buzzerPin = 7;
 
 // *** OTHER VARIABLES
-
 const float PressureSensorOffset = 88;
 float pressure;               // Measured pressure from spx? sensor. (0-100 mbar)
 float  pressureLV = -5;      // Pressure lower value
 float  pressureUV = 50;      // Pressure upper value (100). 50 mbar to be able to use breath to set alarm
-int pressurePeriod;           // Period between pressure spikes. (2 - 6 s)
+float pressurePeriod;           // Period between pressure spikes. (2 - 6 s)
 int  pressurePeriodLV = 2;    // Period lower value
 int  pressurePeriodUV = 6;   // Perid upper value 
 float batteryVoltage;         // Battery voltage. (0-30 V)
@@ -47,8 +54,31 @@ unsigned long buzzerFreq = 500;            // Buzzer frequency
 #define OLED_RESET     4 // Reset pin # (or -1 if sharing Arduino reset pin)
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
-// Also declare plotter as global
-Plotter p;
+float getMean (float inptArray[]){
+    float s;
+    s = 0;
+
+    for ( int i = 0; i <lag; i++)
+    {
+        s = s+ inptArray[i];
+    }
+
+    return s/lag;
+}
+
+float getSTD (float inptArray[], float mean){
+    float s;
+    s = 0;
+
+    for ( int i = 0; i <lag; i++)
+    {
+        s = s + (abs(inptArray[i] - mean) * abs(inptArray[i] - mean));
+    }
+
+    s = sqrt(s/(lag+1));
+
+    return s;
+}
 
 void printOLED(int X,int Y, String Text){
   display.setCursor(X,Y);
@@ -58,10 +88,6 @@ void printOLED(int X,int Y, String Text){
 
 void setup() {
   Serial.begin(9600); // initialize serial
-
-  p.Begin(); // initilize plotter
-  p.AddTimeGraph( "5 variable time graph", 3000, "Pressure (kPa)", pressure);
-
   pinMode (rLedPin, OUTPUT);
   pinMode (yLedPin, OUTPUT);
   pinMode (gLedPin, OUTPUT);
@@ -93,7 +119,6 @@ void setup() {
 }
 
 void loop() {
-  
   // Read pressure sensor MPX5010DP
     pressure = analogRead(pressurePin); // Raw analog value
     pressure = pressure * 5/1023; // Analog value to Voltage
@@ -102,7 +127,7 @@ void loop() {
     pressure = pressure + 0; // Offset, (what is our offset ? )
 
   // Read virtual parameters from potMeters
-  pressurePeriod = map(analogRead(pressurePeriodPin), 1023, 0, 0, 10);  // Read virtual frequency
+  // pressurePeriod = map(analogRead(pressurePeriodPin), 1023, 0, 0, 10);  // Read virtual frequency
   batteryVoltage = map(analogRead(batteryVoltagePin), 1023, 0, 0, 30);  // Read virtual battery Voltge
   xPower = map(analogRead(xPowerPin), 1023, 0,0,10); // Read main power input
 
@@ -115,10 +140,9 @@ void loop() {
     printOLED(0,27,String("Power: " + String(xPower)));
     lastUpdate = millis();
   }
- 
-  
+
   // Check threshold values for pressure
-  if ((pressure > pressureUV) || (pressure < pressureLV) || (pressurePeriod > pressurePeriodUV) || (pressurePeriod < pressurePeriodLV)) {
+  if ((pressure > pressureUV) || (pressure < pressureLV) || (((millis()-lastInsp)/1000) > pressurePeriodUV) || (((millis()-lastInsp)/1000) < pressurePeriodLV)) {
     digitalWrite (rLedPin, HIGH);
     xPressureOK = false;
   }
@@ -146,9 +170,34 @@ void loop() {
 
 
 if (!xPowerOK || !xPressureOK) {
-  toneAC(300, 10, 100, false); // Play thisNote at full volume for noteDuration in the background.
+  // toneAC(300, 10, 100, false); // Play thisNote at full volume for noteDuration in the background.
 }
 
-// p.Plot();
-// Serial.println(pressure);
+// Create signal based on Z-score value
+
+mean = getMean(floatArray); // get mean
+stdev = getSTD(floatArray, mean); // get stdev
+
+  if ((abs(pressure-mean) > abs(threshold * stdev)) && ((lastInsp + 1500) < millis()) && (pressure > mean)) {
+    signal = 1;
+    pressurePeriod = (millis()-lastInsp)/1000;
+    lastInsp = millis();
+  }
+  else {
+    signal = 0;
+  }
+// // } 
+
+// Fill vector with latest pressure value 
+for (int i = 0 ; i <lag ; i++){
+  floatArray[i] = floatArray[i+1];
+}
+
+floatArray[lag]  = pressure;
+
+Serial.print(signal);
+Serial.print("\t");
+Serial.print(pressure);
+Serial.print("\t");
+Serial.println(mean);
 }
